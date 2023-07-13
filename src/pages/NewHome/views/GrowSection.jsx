d0a348dd68c3521c9d8b62c9b22c68975adf58e9
Dayo <abegunde.michael2@gmail.com>
2023-07-13 14:09:13 +0100

    add phone and email to header

--- src/pages/NewHome/views/GrowSection.jsx
+++ src/pages/NewHome/views/GrowSection.jsx
@@ -4,10 +4,8 @@ import MezovestLogo from "../../../assets/icons/mezovest-white-logo.svg"
 import Navigator from '../components/Navigator';
 import HeroSection from '../components/HeroSection';
 import MezHeroImg from "../../../assets/icons/mez-hero-img.svg"
-import MetlLogoIcon from "../../../assets/icons/metl-logo-white.svg";
-import MezoPayIcon from "../../../assets/icons/mezo-pay-white.svg";
-import MezoPayLimitedIcon from "../../../assets/icons/mezo-pay-limited-icon.svg";
-import MezoCoinIcon from "../../../assets/icons/mezocoin-logo-white.svg";
+import EmailIcon from "../../../assets/icons/email-icon.svg";
+import CallIcon from "../../../assets/icons/call-icon.svg";
 
 
 function GrowSection() {
@@ -17,6 +15,22 @@ function GrowSection() {
       <div className='py-7 md:pt-11 md:pb-36 px-6 xl:pl-36  !bg-darkGreen w-full md:w-1/2 flex flex-col'>
         <a className='flex justify-between' href="/">
           <img src={MezovestLogo} alt="Mezovest Logo" className='h-[40px]' />
+          <div className='hidden md:flex'>
+          <a href="mailto:hello@mezovest.com" className='flex cursor-pointer'>
+              <img src={EmailIcon} alt="mezovest email icon" />
+              <div className='text-white ml-2'>
+                <p className='font-semibold'>Email us</p>
+                <p className='text-xs'>hello@mezovest.com</p>
+              </div>
+          </a>
+          <a href="tel:+234 812 345 6789" className='flex cursor-pointer ml-6'>
+              <img src={CallIcon} alt="mezovest email icon"/>
+              <div className='text-white ml-2'>
+                <p className='font-semibold'>Call Us</p>
+                <p className='text-xs'>09133577245</p>
+              </div>
+          </a>
+        </div>
         </a>
         <Navigator />
         <HeroSection />
